0472434a5749712679173218f3990a4ac0417f12
ARUNABH JAIN <btech19eskcs046@skit.ac.in>
2022-08-08 21:21:09 +0530

    adjusted frontend items

--- ui/src/Components/Header.js
+++ ui/src/Components/Header.js
@@ -2,13 +2,15 @@ import "./Stylesheets/Header.css";
 import Modalbox from '../Components/Modalbox';
 import Searchbar from "../Components/Searchbar";
 const Header = () => (
-    <div className="head">
-        <div className="image">
-            <img src="https://www.decurtis.com/wp-content/uploads/2019/11/logo-decurtis-corporation.png" alt="logo" className="logo"/>
+    <div className="header">
+        <div className="head">
+            <div className="image">
+                <img src="https://www.decurtis.com/wp-content/uploads/2019/11/logo-decurtis-corporation.png" alt="logo" className="logo"/>
+            </div>
+            <h1>SRE INSIGHTS ADMIN</h1>
+            <Searchbar/>
+            <Modalbox/>
         </div>
-        <h1>SRE INSIGHTS ADMIN</h1>
-        <Searchbar/>
-        <Modalbox/>
     </div>
 );
 export default Header;
