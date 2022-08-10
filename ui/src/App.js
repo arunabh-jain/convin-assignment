2f56d240e0f32a810629e19d349a453482a61f63
Akshat Gadodia <akshatgadodia@gmail.com>
2022-08-11 02:24:11 +0530

    history page removed

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
 import Homepage from './Pages/Homepage';
-import History from './Pages/History';
-import PageNotFound from './Pages/PageNotFound';
-import { BrowserRouter,Routes,Route, } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage/>} />
-        <Route path="history" element={<History />} />
-        <Route path="*" element={<PageNotFound/>} />
-      </Routes>
-    </BrowserRouter>
+    <Homepage/>
   );
 }
 
