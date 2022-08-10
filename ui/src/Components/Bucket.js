0eb9c6aec1cc2e2f3c2e5ff61f5bc8f5760737e9
Akshat Gadodia <akshatgadodia@gmail.com>
2022-08-11 02:11:17 +0530

    TEST

--- ui/src/Components/Bucket.js
+++ ui/src/Components/Bucket.js
@@ -6,9 +6,53 @@ import VideoCard from "../Components/VideoCard";
 import Modalbox from '../Components/Modalbox';
 
 const Bucket = (props) => {
+  const[elementsToDelete, setElementsToDelete] = useState([])
   const [videoData,setVideoData] = useState([]);
-  useEffect(() => {setVideoData(props.videos);}, [props.videos]);
+
+  function GetSortOrder() {    
+    return function(a, b) {   
+        if (a.video_name > b.video_name) {    
+            return 1;    
+        } else if (a.video_name < b.video_name) {    
+            return -1;    
+        }    
+        return 0;    
+    }    
+  }   
+  useEffect(() => {setVideoData(props.videos.sort(GetSortOrder()));}, [props.videos]);
   
+  const addElementsToDelete = (videoID) => {
+    setElementsToDelete(oldArray => [...oldArray, videoID])
+  }
+
+  const deleteElementsToDelete = () => {
+    setElementsToDelete([]);
+  }
+
+  const removeElementsToDelete = (videoID) => {
+    setElementsToDelete(elementsToDelete.filter(value=>{return value!==videoID}))
+  }
+
+  const deleteElements = async() => {
+    let response = await fetch(`http://localhost:8000/buckets/${props.id}`,{method: "GET"});
+    let data = await response.json();
+    data.videos = data.videos.filter((value)=>{
+      return !elementsToDelete.includes(value.video_id)
+    })
+    setElementsToDelete([]);
+    await fetch(`http://localhost:8000/buckets/${props.id}`, {
+        method: 'PUT', mode: 'cors',credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+    response = await fetch("http://localhost:8000/buckets",{method: "GET"});
+    response = await response.json();
+    props.setData(response);
+  }
+
   return (
   <div className="bucket-outer">
     <div className="bucket-header">
@@ -18,13 +62,16 @@ const Bucket = (props) => {
     <div className="bucket-body">
       {     
         videoData.map((data)=>{
-          return(<VideoCard key={data.video_id} setData={props.setData} id={data.video_id} name={data.video_name} link={data.video_link} bucketID={props.id} bucketName={props.title}/>)
+          return(<VideoCard key={data.video_id} setData={props.setData} id={data.video_id} name={data.video_name} 
+                            link={data.video_link} bucketID={props.id} bucketName={props.title}
+                            addElementsToDelete={addElementsToDelete} removeElementsToDelete={removeElementsToDelete}
+                            deleteElementsToDelete={deleteElementsToDelete}/>)
         })
       }
       </div>
     <div className="bucket-footer">  
-      <Modalbox id={props.id} setData={props.setData} btnName="ADD"/>
-      <Button size="middle" className="bucket-btn" type="primary" onClick={console.log("Deleted")}>DELETE</Button>
+      <Modalbox id={props.id} setData={props.setData} btnName="ADD" deleteElementsToDelete={deleteElementsToDelete}/>
+      <Button size="middle" className="bucket-btn" type="primary" onClick={deleteElements}>DELETE</Button>
     </div>
   </div>
 )};
--- ui/src/Components/TabSwitch.js
+++ ui/src/Components/TabSwitch.js
@@ -16,15 +16,23 @@ const TabSwitch = () => {
         const response = await fetch("http://localhost:8000/buckets",{method: "GET"});
         const data = await response.json();
         setData(data);
-        console.log(data)
       }
       fetchData();
     }, [])
 
     return(
-        <Fragment>
+    <Fragment>
+        <div className="header">
+        <div className="head">
+        <div className="image">
+                <img src="https://uploads-ssl.webflow.com/606c4d4ed5ba7ca3bc774c4c/606c5fcd82475240c607a79f_LOGO.svg" alt="logo" className="logo"/>
+        </div>
+        <h1>VIDEO BUCKET LIST</h1>
+        </div>
+        </div>
+        
     <Tabs title="VIDEO BUCKET LIST" defaultActiveKey="1" onChange={onChange}>
-    <TabPane className="home-tab" tab="Home" key="1">
+    <TabPane tab="Home" key="1">
         <div className="tab-body">
             {
                 data.map((data)=>{
@@ -33,21 +41,11 @@ const TabSwitch = () => {
             }
         </div>
     </TabPane>
-    <TabPane className="history-tab" tab="History" key="2">
+    <TabPane tab="History" key="2">
             <HistoryTab/>
     </TabPane>
 </Tabs>
-
-<div className="header">
-        <div className="head">
-        <div className="image">
-                <img src="https://uploads-ssl.webflow.com/606c4d4ed5ba7ca3bc774c4c/606c5fcd82475240c607a79f_LOGO.svg" alt="logo" className="logo"/>
-        </div>
-        <p>VIDEO BUCKET LIST</p>
-        </div>
-        </div>
-        </Fragment>
-
+</Fragment>
 );
 }
 export default TabSwitch;
